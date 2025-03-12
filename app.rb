ec69d708f2678a994b1d64a265e97c91de5270ff
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-12 22:59:52 +0000

    dice roll

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+def roll_dice(num_dice, num_sides)
+  Array.new(num_dice) { rand(1..num_sides) }
+end
+
+get '/' do
+  erb (:index)
+end
+
+get '/process_roll' do
+  @num_dice = params[:dice].to_i
+  @num_sides = params[:sides].to_i
+  @rolls = roll_dice(@num_dice, @num_sides)
+  erb (:result)
 end
